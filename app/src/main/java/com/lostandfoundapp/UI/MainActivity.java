923ab366e8f81736008645992baaa7c55500fbcc
Patrick <mussi_kat@hotmail.com>
2018-04-24 12:13:12 +0200

    Initial commit

---
+++ app/src/main/java/com/lostandfoundapp/UI/MainActivity.java
@@ -0,0 +1,53 @@
+package com.lostandfoundapp.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.lostandfoundapp.R;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      sendMessage();
+      getData();
+        textView = (TextView) findViewById(R.id.textItems);
+
+    }
+    public void sendMessage(){
+
+
+        myRef.setValue("Hello, you stupid database!");
+    }
+
+    public void getData(){
+        myRef.addValueEventListener(new ValueEventListener() {
+
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                textView.setText(value);
+
+
+            }
+
+
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+
+            }
+        });
+    }
+}
