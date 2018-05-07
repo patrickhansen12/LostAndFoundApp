ac14048b0608bb5f9c3b39482315a4429e2d308e
CrystalOfLife <nicolai7@godmail.dk>
2018-05-07 11:08:51 +0200

    primitive login

--- app/src/main/java/com/lostandfoundapp/UI/LoginActivity.java
+++ app/src/main/java/com/lostandfoundapp/UI/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.lostandfoundapp.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.lostandfoundapp.R;
 
@@ -10,10 +15,41 @@ import com.lostandfoundapp.R;
  */
 
 public class LoginActivity extends AppCompatActivity {
+    Button Back, Login;
+    TextView FailText;
+    EditText Username, Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activty_login);
+        Back = findViewById(R.id.back);
+        Login = findViewById(R.id.login);
+        FailText = findViewById(R.id.failText);
+        Username = findViewById(R.id.username);
+        Password = findViewById(R.id.password);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+                {
+                    if (Username.getText().toString().equals("1") && Password.getText().toString().equals("1"))
+                    {
+                        Intent x = new Intent();
+                        x.setClass(LoginActivity.this, AddActivity.class);
+                        startActivity(x);
+                    }
+                    else
+                    {
+                        FailText.setText("Brugernavnet eller eller kodeordet er forkert");
+                    }
+                }
+        });
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                onBackPressed();
+            }
+        });
     }
 }
