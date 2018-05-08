38beac75966ea87927893040c55cf852a447b0fb
Patrick <mussi_kat@hotmail.com>
2018-05-08 11:16:55 +0200

    Det er noget lort
    Upload to storage picture not showing

--- app/src/main/java/com/lostandfoundapp/UI/AddActivity.java
+++ app/src/main/java/com/lostandfoundapp/UI/AddActivity.java
@@ -1,5 +1,6 @@
 package com.lostandfoundapp.UI;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -7,6 +8,7 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -15,9 +17,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.lostandfoundapp.BLL.PictureAdapter;
 import com.lostandfoundapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.io.ByteArrayOutputStream;
+import java.util.UUID;
 
 /**
  * Created by Nicolai on 03-05-2018.
@@ -25,18 +38,28 @@ import com.lostandfoundapp.R;
 
 public class AddActivity extends AppCompatActivity {
     private final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-
+private FirebaseStorage storage = FirebaseStorage.getInstance();
     PictureAdapter pictureAdapter;
 
     Button PictureButton, SaveButton, BackButton;
+   View imageContainer;
     ImageView Picture;
     EditText NameText;
     Spinner Dropdown;
     Bitmap itemPicture;
+ private StorageReference mStorage;
+ private ProgressDialog mProgress;
+ TextView overlayText;
+ private TextView downloadedUrl;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mStorage = FirebaseStorage.getInstance().getReference();
         super.onCreate(savedInstanceState);
+        imageContainer = findViewById(R.id.image_container);
+        overlayText = (TextView) findViewById(R.id.overlayText);
         setContentView(R.layout.activity_add);
         PictureButton = findViewById(R.id.pictureButton);
         SaveButton = findViewById(R.id.saveButton);
@@ -44,19 +67,22 @@ public class AddActivity extends AppCompatActivity {
         Picture = findViewById(R.id.picture);
         NameText = findViewById(R.id.nameText);
         Dropdown = findViewById(R.id.dropdown);
-
+mProgress = new ProgressDialog(this);
+        downloadedUrl = (TextView) findViewById(R.id.download_url);
         PictureButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                {
-                    openPictureActivity();
-                }
+openPictureActivity();
+
             }
         });
+
     }
+
 
     public void openPictureActivity() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -67,8 +93,32 @@ public class AddActivity extends AppCompatActivity {
                     break;
             }
         }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
+           // mProgress.setMessage("Uploading Image ....");
+            //mProgress.show();
+
+            Picture.setDrawingCacheEnabled(true);
+            Picture.buildDrawingCache();
+            Bitmap bitmap = Picture.getDrawingCache();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//            bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            Picture.setDrawingCacheEnabled(false);
+            byte[] data1 = baos.toByteArray();
+            String path = "items/" + UUID.randomUUID() + ".png";
+            StorageReference itemsRef = storage.getReference(path);
+
+            StorageMetadata metadata = new StorageMetadata.Builder().setCustomMetadata("text", NameText.getText().toString()).build();
+            UploadTask uploadTask = itemsRef.putBytes(data1, metadata);
+            uploadTask.addOnSuccessListener(AddActivity.this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri url = taskSnapshot.getDownloadUrl();
+                    downloadedUrl.setText(url.toString());
+                    downloadedUrl.setVisibility(View.VISIBLE);
+                }
+
+    });
+            super.onActivityResult(requestCode, resultCode, data);
+}
 
     public void setTakenPicture(Intent data) {
         Bitmap picture = (Bitmap) data.getExtras().get("data");
@@ -94,6 +144,10 @@ public class AddActivity extends AppCompatActivity {
         canvas.drawBitmap(bitmap, rect, rect, paint);
         return output;
     }
+    }
 
-}
+
+
+
+
 
